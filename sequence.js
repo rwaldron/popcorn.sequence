08a1ca2fc10549be7ed70bb27054e196b122cc86
rwldrn <waldron.rick@gmail.com>
2011-04-08 16:48:31 -0400

    clip support

--- sequence.js
+++ sequence.js
@@ -1,138 +1,211 @@
 //  Requires Popcorn.js
 (function( global, Popcorn ) {
+
+	//	TODO: as support increases, migrate to element.dataset 
   
   var doc = global.document, 
-      rprotocol = /:\/\//;
+      rprotocol = /:\/\//, 
+      //  TODO: better solution to this sucky stop-gap
+      lochref = location.href.replace( location.href.split("/").slice(-1)[0], "" );
 
-  Popcorn.sequence = function( mediaId, list ) {
-    return new Popcorn.sequence.init( mediaId, list );
+  Popcorn.sequence = function( place, list ) {
+    return new Popcorn.sequence.init( place, list );
   };
  
-  Popcorn.sequence.init = function( mediaId, list ) {
+  Popcorn.sequence.init = function( place, list ) {
     
     //  Video element
-    this.video = doc.getElementById( mediaId );
+    this.place = doc.getElementById( place );
     
     //  Store ref to a special ID
-    this.seqId = Popcorn.guid( "__sequenced"  );
-    
-    //  Store ref to the video's parent
-    this.parent = this.video.parentNode;
-    
+    this.seqId = Popcorn.guid( "__sequenced" );
+
     //  Create the queue
     this.queue = [];
-    
-    //  Push the initial target video into the queue
-    this.queue.push( this.video );
+
+    //  Create the clips
+    this.clips = [];
     
     //  Create the Popcorn object list
     this.playlist = [];
     
     //  Create dimensions store    
     this.dims = {
-      width: this.video.videoWidth,
-      height: this.video.videoHeight
+      width: 0, //this.video.videoWidth,
+      height: 0 //this.video.videoHeight
     };
-    
+
+    this.active = 0;
+
     var self = this;
-    
+
     //  Create `video` elements
-    Popcorn.forEach( list, function( src, idx ) {
+    Popcorn.forEach( list, function( media, idx ) {
       
-      var media;
-      
-      if ( !idx ) {
-        
-        media = self.video;
+      var video = doc.createElement( "video" ), 
 
-      } else {
-        
-        media = doc.createElement( "video" );
-        
-        media.controls = true;
+      		//	Context sensitive callbacks
+          loadedMetaData = function( event ) {
 
-        media.style.display = "none";
-        
-        media.id = self.seqId + "-" + idx ;
-        
-        self.queue.push( media );
-        
-        self.parent.appendChild( media );
-        
-      }
-      
-      media.src = !rprotocol.test( src ) ? location.href + src : src;
-      
-      media.setAttribute("data-sequence-owner", mediaId );
-      media.setAttribute("data-sequence-guid", self.seqId );
-      media.setAttribute("data-sequence-id", idx );
+          	var target = event.srcElement;
 
-    });
-    
-    //  Listen for cue to switch out videos 
-    Popcorn.forEach( this.queue, function( media, idx ) {
+            //  If no out point was set for this clip, set it to the duration of the video
+            self.clips[ idx ].out = ( !self.clips[ idx ].out && target.duration ) || self.clips[ idx ].out;
+
+            //  If this is idx zero, use it as dimension for all
+            if ( !idx ) {
+              self.dims.width = event.srcElement.videoWidth;
+              self.dims.height = event.srcElement.videoHeight;
+            }
+            
+            video.removeEventListener( "loadedmetadata", loadedMetaData, false );
+          }, 
+          canPlayThrough = function( event ) {
+
+            self.queue[ idx ].currentTime = self.clips[ idx ].in;
+
+            self.queue[ idx ].setAttribute("data-sequence-id", [ self.clips[ idx ].in, self.clips[ idx ].out ].join(":") );
+
+            video.removeEventListener( "canplaythrough", canPlayThrough, false );
+          };
+
+
+      //  Setup newly created video element
+      video.controls = true;
+
+      //  If the first, show it, if the after, hide it
+      video.style.display = ( idx && "none" ) || "" ;
+
+      //  Seta registered sequence id
+      video.id = self.seqId + "-" + idx ;
+
+      //  Push this video into the sequence queue
+      self.queue.push( video );
+
+      //  Push the in/out points into sequence clips
+      self.clips.push({ 
+        in: ( "in" in media ) && media.in || 0,
+        out: ( "out" in media ) && media.out || 0,  
+      });
+
+      //  Append the video to the place holder element
+      self.place.appendChild( video );
+
+      //  Set the sources
+      video.src = !rprotocol.test( media.src ) ? lochref + media.src : media.src;
+
+      //  Set some squence specific data vars
+      video.setAttribute("data-sequence-owner", place );
+      video.setAttribute("data-sequence-guid", self.seqId );
+      video.setAttribute("data-sequence-id", idx );
       
-      
-      media.addEventListener( "ended", function() {
-      
-        var nextId = 0;
-      
-        //  Update to current correct dimensions        
+      //  Hook up event listeners for managing special playback 
+      video.addEventListener( "loadedmetadata", loadedMetaData, false);
+
+      video.addEventListener( "canplaythrough", canPlayThrough, false );
+
+      video.addEventListener( "play", function( event ) {
+
+        var target = event.srcElement;
+        
+        if ( target.currentTime >= self.clips[ idx ].out || 
+        			target.currentTime < self.clips[ idx ].in  ) {
+
+//setAttribute("data-sequence-id"
+          console.log( "target play event", target, target.currentTime, self.clips[ idx ].out );
+          target.currentTime =  self.clips[ idx ].in;
+        }
+
+      }, false );
+
+			video.addEventListener( "timeupdate", function( event ) {
+
+				var target = event.srcElement, 
+						;
+				
+				if ( target.currentTime >= )
+				
+			}, false );
+			
+      video.addEventListener( "ended", function( event ) {
+
+      	console.log( event.srcElement.src );
+
+        var nextId = 0, 
+        		next, clip;
+
+        //  Update to current correct dimensions
         if ( !self.dims.width ) {
-          self.dims.width = media.videoWidth;
-          self.dims.height = media.videoHeight;
+          self.dims.width = self.dims.width || media.videoWidth;
+          self.dims.height = self.dims.height || media.videoHeight;
         }
-      
-        if ( self.queue[ idx + 1 ] ) {
-          nextId = idx + 1;
-        }
-        
-        //  Reset queue
-        if ( !self.queue[ idx + 1 ] ) {
-          nextId = 0;
-        }
-        
 
-        //  Hide the currently ending video
-        media.style.display = "none";
+        Popcorn.sequence.cycle( self.queue, self.clips, idx );
 
-        //  Constrain dimentions
-        Popcorn.extend( self.queue[ nextId ], {
-          width: self.dims.width, 
-          height: self.dims.height
-        });          
-
-        //  Show the next video in the sequence    
-        self.queue[ nextId ].style.display = "";
-        
-        //  When not resetting to 0
-        if ( !!nextId ) {
-          //  Play the next video in the sequence
-          self.queue[ nextId ].play();
-        }
-        
-        //  When reseting to first video
-        if ( !nextId ) {
-          //  Reset currentTime to 0
-          self.queue[ nextId ].currentTime = 0;
-        }
-        
       }, false);
-      
+
       //  Add a Popcorn object instance to the 
       //  playlist video list
-      self.playlist.push( Popcorn("#" + media.id) );
-      
+      self.playlist.push( Popcorn("#" + video.id) );
+
     });
-    
+
     return this;
   };
-  
+
   Popcorn.sequence.init.prototype = Popcorn.sequence.prototype;
+
+  Popcorn.sequence.cycle = function( queue, clips, currentIdx ) {
+
+		var current = queue[ currentIdx ], 
+				nextIdx, next, clip;
+
+
+    if ( queue[ currentIdx + 1 ] ) {
+      nextIdx = currentIdx + 1;
+    }
+
+    //  Reset queue
+    if ( !queue[ currentIdx + 1 ] ) {
+      nextIdx = 0;
+    }
+
+		next = queue[ nextIdx ];
+		clip = clips[ nextIdx ];
+
+
+    //  Hide the currently ending video
+    current.style.display = "none";
+
+//    //  Constrain dimentions
+//    Popcorn.extend( next, {
+//      width: self.dims.width, 
+//      height: self.dims.height
+//    });
+
+    //  Show the next video in the sequence    
+    next.style.display = "";
+
+    //  When not resetting to 0
+    if ( !!nextIdx ) {
+
+			next.currentTime = clip.in;
+    
+      //  Play the next video in the sequence
+			next.play();
+    }
+
+    //  When reseting to first video
+    if ( !nextId ) {
+      //  Reset currentTime to 0
+      next.currentTime = clip.in;
+    }
   
+  };
+
   //  Sequence object prototype
   Popcorn.extend( Popcorn.sequence.prototype, {
-  
+
     //  Returns Popcorn object from sequence at index
     eq: function( idx ) {
       return this.playlist[ idx ];
@@ -149,6 +222,12 @@
       }
       
       return ret;
+    }, 
+
+    play: function() {
+
+      console.log( this );
+   
     }, 
     
     //  Binds event handlers that fire only when all 
@@ -168,7 +247,6 @@
           }
           
         });
-      
       });
     }
   });
--- test/sequence.functional.js
+++ test/sequence.functional.js
@@ -1,7 +1,34 @@
 (function( global, Popcorn ) {
-
-
-
-
-
 })( this, Popcorn );
+
+
+
+
+$(function() {
+
+
+	var localMediaObjs = [
+		    { 
+		    	src: "assets/snowdriving.ogv", 
+		    	in: 22
+		    }, 
+		    {
+		    	src: "assets/eich.ogv",
+		    	in: 5, 
+		    	out: 10
+		    }, 
+		    {
+					src: "assets/crockford.ogv", 
+					out: 10
+		    }
+		  ],  
+			$seq;
+
+	$seq = Popcorn.sequence( "seq-fixture", localMediaObjs );
+
+
+	$seq.play();
+
+	window.$seq = $seq;
+
+});
